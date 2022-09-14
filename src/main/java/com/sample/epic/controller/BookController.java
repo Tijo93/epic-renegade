37aaf734061276f9e36f5f3f81ec0d96265367bd
tijo93thomas@gmail.com <tijo.thomas@sanesquare.com>
2022-09-14 13:11:16 +0530

    updates

--- src/main/java/com/sample/epic/controller/BookController.java
+++ src/main/java/com/sample/epic/controller/BookController.java
@@ -17,27 +17,27 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @GetMapping
+    @GetMapping // http://localhost:8899/api/books/
     public ResponseEntity<List<Book>> getBooks() {
         return ResponseEntity.ok(bookService.getBooks());
     }
 
-    @GetMapping("{id}")
+    @GetMapping("{id}") //http://localhost:8899/api/books/n
     public ResponseEntity<Book> getBook(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.getBook(id));
     }
 
-    @PostMapping
+    @PostMapping // http://localhost:8899/api/books/
     public ResponseEntity<Book> saveBook(@RequestBody @Validated BookDTO bookDTO) {
         return ResponseEntity.ok(bookService.saveBook(bookDTO));
     }
 
-    @PutMapping
+    @PutMapping // http://localhost:8899/api/books
     public ResponseEntity<Book> updateBook(@RequestBody @Validated BookDTO bookDTO) {
         return ResponseEntity.ok(bookService.updateBook(bookDTO));
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("{id}") //http://localhost:8899/api/books/n
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
         return ResponseEntity.ok().build();
